d58628f6f68928691e52c73ed8d88b13f3f21c3a
Arjun Rai <arai88292@gmail.com>
2025-03-12 19:21:55 +0530

    index.js changed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('New Feature!')
 })
 app.get('/twitter', (req, res) => {
     res.send('This is a node server')
